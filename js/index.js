ef30dff80a7294b9ed9cb4716c47e9da2365fe8a
Nathan <nathan.lib19@gmail.com>
2021-02-13 14:56:32 +0100

    custom switch for dark/lightmode

--- js/index.js
+++ js/index.js
@@ -2,10 +2,19 @@ window.addEventListener(
 	"load",
 	function () {
 		var navToggler = document.getElementById("nav-toggler");
+		var switchMode = document.getElementById("switch-mode");
+
 		navToggler.addEventListener("click", function () {
 			var menu = document.getElementById("menu-toggle");
 			menu.classList.toggle("display-none");
 		});
+
+		// switchMode.addEventListener("click", function () {
+		// 	var switchModeLabel = document.getElementById("switch-mode-label");
+		// 	switchModeLabel.innerHTML = switchMode.checked
+		// 		? "Light mode"
+		// 		: "Dark mode";
+		// });
 	},
 	false
 );
