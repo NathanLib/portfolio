afae478622a5b31e4d982129dbe83c525994b97d
Nathan <nathan.lib19@gmail.com>
2021-02-13 22:03:36 +0100

    remove fullpage files

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
-import fullpage from "fullpage.js";
 
 $(window).on("load", function () {
-	// -----------
-	new fullpage("#fullpage", {
-		licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-		navigation: true,
-		sectionsColor: ["#ff5f45", "#0798ec", "#fc6c7c", "grey"],
-	});
-	// -----------
-
 	$("#nav-toggler").on("click", () => {
 		$("#menu-toggle").toggleClass("display-none");
 	});
